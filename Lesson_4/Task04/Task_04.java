5c3d47b22c2ff55057473a042db08d08167cad18
kikamazafuka <uncelfuka@gmail.com>
2019-05-26 20:34:08 +0300

    Update Task_04.java

--- Lesson_4/Task04/Task_04.java
+++ Lesson_4/Task04/Task_04.java
@@ -5,6 +5,8 @@ public class Task_04 {
     public static void main(String[] args) {
 
         double a = CalcLogic.calcArith(123456);
+        double b = CalcLogic.calcGeom(123456);
         Printer.print(a);
+        Printer.print(b);
     }
 }
