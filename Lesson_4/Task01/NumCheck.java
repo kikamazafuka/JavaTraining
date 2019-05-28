736bc027c5b07c124d07cd8472bc9f7dc8c8c389
kikamazafuka <uncelfuka@gmail.com>
2019-05-28 22:34:41 +0300

    Update NumCheck.java

--- Lesson_4/Task01/NumCheck.java
+++ Lesson_4/Task01/NumCheck.java
@@ -3,4 +3,8 @@ public class NumCheck {
     public static boolean isEqual(int a, int b, int c) {
         return a == b && b == c;
     }
+    
+        public static boolean isNotEqual(int a, int b, int c) {
+        return a != b || b != c || a != c;
+    }
 }
