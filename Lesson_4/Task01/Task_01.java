1e4d371a46a57d9a52400abdc6c1db17f53b3f88
kikamazafuka <uncelfuka@gmail.com>
2019-05-28 23:23:23 +0300

    Update Task_01.java

--- Lesson_4/Task01/Task_01.java
+++ Lesson_4/Task01/Task_01.java
@@ -2,12 +2,13 @@ public class Task_01 {
 
     public static void main(String[] args) {
 
-       int a = 1;
-       int b = 1;
-       int c = 1;
-       boolean d = NumCheck.isEqual(a, b, c);
-       boolean e = NumCheck.isNotEqual(a, b, c);
-       Printer.print(d);
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        boolean d = NumCheck.isEqual(a, b, c);
+        boolean e = NumCheck.isNotEqual(a, b, c);
+        Printer.print("Compared numbers are equal: " + d);
+        Printer.print("Compared numbers are not equal: " + e);
 
     }
 }
