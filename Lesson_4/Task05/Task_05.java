6c0bc2364574a93a6e1a746ff4457e1d3d8566c4
kikamazafuka <uncelfuka@gmail.com>
2019-05-29 00:17:50 +0300

    Update Task_05.java

--- Lesson_4/Task05/Task_05.java
+++ Lesson_4/Task05/Task_05.java
@@ -5,7 +5,9 @@ public class Task_05 {
     public static void main(String[] args) {
 
         int number = 1234567;
-        int a = ReverseLogic.reverse(number);
-        Printer.print(a);
+        /*int a = ReverseLogic.reverse(number);
+        Printer.print(""+a);*/
+        String b = ReverseLogic.reverse2(number);
+        Printer.print("Reverse number :" + b);
     }
 }
