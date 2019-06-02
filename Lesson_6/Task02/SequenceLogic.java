5606e0cbbbacfbf5f064439f045b56641902df32
kikamazafuka <uncelfuka@gmail.com>
2019-06-02 23:49:58 +0300

    Add files via upload

---
+++ Lesson_6/Task02/SequenceLogic.java
@@ -0,0 +1,71 @@
+package by.epam.javatraining.artsem.lesson_06.task_02;
+
+public class SequenceLogic {
+
+    public static boolean isUpSeq(int number){
+        int lastDigit= number%10;
+        boolean flag = true;
+        while (number>0){
+            number/=10;
+            int nextDig = number%10;
+            if (nextDig>=lastDigit){
+                flag = false;
+            }
+            lastDigit=nextDig;
+        }
+        return flag;
+    }
+
+    public static boolean isDownSeq(int number){
+        int lastDigit= number%10;
+        boolean flag = true;
+        while (number>0){
+            number/=10;
+            int nextDig = number%10;
+            if (nextDig<=lastDigit){
+                flag = false;
+            }
+            lastDigit=nextDig;
+        }
+        return flag;
+    }
+
+    public static boolean isIncrSeq (int number) {
+        String temp = Integer.toString(number);
+        int[] arr = new int[temp.length()];
+        boolean flag = true;
+        for (int i = 0; i < temp.length()-1; i++) {
+            arr[i] = temp.charAt(i);
+            arr[i] = Character.getNumericValue(arr[i]);
+        }
+        for (int i = 0; i<arr.length-1; i++){
+            if (arr[i]>=arr[i+1]){
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
+    public static int [] getArr (int number){
+        String temp = Integer.toString(number);
+        int[] arr = new int[temp.length()];
+        boolean flag = true;
+        for (int i = 0; i < temp.length()-1; i++) {
+            arr[i] = temp.charAt(i);
+            arr[i] = Character.getNumericValue(arr[i]);
+        }
+        return arr;
+    }
+
+    public static boolean isDecSeq (int [] arr){
+
+        boolean flag = true;
+        for (int i = 0; i<arr.length-1; i++){
+            if (arr[i]<=arr[i+1]){
+                flag = false;
+            }
+        }
+
+        return flag;
+    }
+}
---
+++ Lesson_6/Task02/Task_02.java
@@ -0,0 +1,11 @@
+package by.epam.javatraining.artsem.lesson_06.task_02;
+
+import by.epam.javatraining.artsem.lesson_05.task_01.View;
+
+public class Task_02 {
+    public static void main(String[] args) {
+
+        boolean seq = SequenceLogic.isUpSeq(12345);
+        View.print(""+seq);
+    }
+}
