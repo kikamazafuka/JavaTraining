96c7dc2a40d9df75a4e2958cc3ce5933afa7e414
kikamazafuka <uncelfuka@gmail.com>
2019-06-04 23:38:27 +0300

    Update MoodSensorLogic.java

--- Lesson_5/Task_03/MoodSensorLogic.java
+++ Lesson_5/Task_03/MoodSensorLogic.java
@@ -2,12 +2,30 @@ package by.epam.javatraining.artsem.lesson_05.task_03;
 
 public class MoodSensorLogic {
 
-    public static String moodDefine(int number) {
-        if (number < 0) {
-            return "Incorrect mood data, please enter positive number";
+    public static final String LUCKY = ":D";
+    public static final String HAPPY = ":)";
+    public static final String ANGRY = ":(";
+    public static final String CONFUSED = "o.O";
+
+    public static final Random number = new Random();
+    public static final int moodCount = 4;
+
+    public static String guessMood() {
+
+        int a = number.nextInt(moodCount);
+        String mood = HAPPY;
+        switch (a) {
+            case 1:
+                mood = LUCKY;
+                break;
+            case 2:
+                mood = ANGRY;
+                break;
+            case 3:
+                mood = CONFUSED;
         }
-        if (number > 50) {
-            return ":)";
-        } else return ":(";
+
+        return mood;
     }
 }
+
