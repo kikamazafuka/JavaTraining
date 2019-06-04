7c43ff14c694e29b34d25701c17b421b628a8028
kikamazafuka <uncelfuka@gmail.com>
2019-06-04 23:36:53 +0300

    Update Task_02.java

--- Lesson_5/Task_02/Task_02.java
+++ Lesson_5/Task_02/Task_02.java
@@ -5,7 +5,8 @@ import by.epam.javatraining.artsem.lesson_05.task_01.View;
 public class Task_02 {
     public static void main(String[] args) {
 
-        boolean a = LetterLogic.isVovel("ello");
+        boolean a = LetterLogic.isVovelThird('Y');
+        boolean b = LetterLogic.isVovelFourth('a');
         View.print("Letter is vovel: "+a);
     }
 }
