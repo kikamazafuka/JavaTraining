171accc0b7e3df33c39b724f5b664077c4a4a2fe
kikamazafuka <uncelfuka@gmail.com>
2019-06-10 23:30:10 +0300

    refactor

--- maintask01/test/by/epam/javatraining/artsem/maintask01/model/VectorLogicTest.java
+++ maintask01/test/by/epam/javatraining/artsem/maintask01/model/VectorLogicTest.java
@@ -1,3 +1,10 @@
+/** Test Class of VectorLogic class
+ * @author Artur Semenas
+ * @see#VectorLogic class
+ * 
+ * 
+ * 
+ */
 package by.epam.javatraining.artsem.maintask01.model;
 
 import org.junit.Test;
@@ -9,7 +16,7 @@ public class VectorLogicTest {
     @Test
     public void findMax() {
 
-        int [] arr = new int[]{1,2,4,52,3};
+        int[] arr = new int[]{1, 2, 4, 52, 3};
         int expected = 52;
         assertEquals(expected, VectorLogic.findMax(arr));
     }
@@ -17,7 +24,7 @@ public class VectorLogicTest {
     @Test
     public void findMin() {
 
-        int [] arr = new int[]{1,2,4,52,3};
+        int[] arr = new int[]{1, 2, 4, 52, 3};
         int expected = 1;
         assertEquals(expected, VectorLogic.findMin(arr));
     }
@@ -25,30 +32,50 @@ public class VectorLogicTest {
     @Test
     public void findArithMean() {
 
-        double [] arr = new double[]{1,2,3};
+        double[] arr = new double[]{1, 2, 3};
         double expected = 2d;
         double delta = 0.1;
-        assertEquals(expected, VectorLogic.findArithMean(arr),delta);
+        assertEquals(expected, VectorLogic.findArithMean(arr), delta);
     }
 
     @Test
     public void findGeomMean() {
+
+        double[] arr = new double[]{1, 2, 3};
+        double expected = 1.9;
+        double delta = 1;
+        assertEquals(expected, VectorLogic.findArithMean(arr), delta);
     }
 
     @Test
     public void isUpSort() {
+        int[] arr = new int[]{4, 1, 2, 3};
+        boolean expected = false;
+        assertEquals(expected, VectorLogic.isUpSort(arr));
     }
 
     @Test
     public void isDownSort() {
+
+        int[] arr = new int[]{4, 3, 2, 1};
+        boolean expected = true;
+        assertEquals(expected, VectorLogic.isDownSort(arr));
     }
 
     @Test
-    public void findLocMax() {
+    public void findLocMaxPos() {
+
+        int[] arr = new int[]{0, 3, 2, 1};
+        int expected = 1;
+        assertEquals(expected, VectorLogic.findLocMaxPos(arr));
     }
 
     @Test
     public void findLocMin() {
+
+        int[] arr = new int[]{2, 3, 0, 1};
+        int expected = 2;
+        assertEquals(expected, VectorLogic.findLocMinPos(arr));
     }
 
     @Test
