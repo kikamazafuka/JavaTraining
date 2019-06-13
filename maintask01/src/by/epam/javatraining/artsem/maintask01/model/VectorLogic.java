a3489210f24c8ccd4cc1c3321da9984f5f93bb29
kikamazafuka <uncelfuka@gmail.com>
2019-06-14 00:12:48 +0300

    ref, log

--- maintask01/src/by/epam/javatraining/artsem/maintask01/model/VectorLogic.java
+++ maintask01/src/by/epam/javatraining/artsem/maintask01/model/VectorLogic.java
@@ -1,4 +1,4 @@
- /**
+/**
  * Main task 01 v.1.0 by Artur Semenas 09.06.2019
  *
  * @author Artur Semenas
@@ -217,7 +217,7 @@ public class VectorLogic {
     }
 
     //BigO of bubbleSort is O(n^2)
-    
+
     public static void bubbleUpSort(int[] array) {
 
         if (array.length == 0) {
@@ -301,4 +301,51 @@ public class VectorLogic {
         }
     }
 
+    // BigO of binarySearch is O(log(n))
+
+    public static void quickSort(int[] array, int low, int high) {
+
+        if (array.length == 0) {
+            return;
+        }
+
+        if (low >= high) {
+            return;
+        }
+
+        int middle = low + (high - low) / 2;
+        int num = array[middle];
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < num) {
+                i++;
+            }
+
+            while (array[j] > num) {
+                j--;
+            }
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+
+        if (low < j) {
+            quickSort(array, low, j);
+        }
+
+        if (high > i) {
+            quickSort(array, i, high);
+        }
+    }
+
 }
+
+
+
