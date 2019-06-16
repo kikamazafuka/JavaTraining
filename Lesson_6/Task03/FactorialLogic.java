1d14ed2cba2cae8b79f24325dc585463172c5a43
kikamazafuka <uncelfuka@gmail.com>
2019-06-16 13:20:44 +0300

    Update FactorialLogic.java

--- Lesson_6/Task03/FactorialLogic.java
+++ Lesson_6/Task03/FactorialLogic.java
@@ -3,11 +3,11 @@ package by.epam.javatraining.artsem.lesson_06.task_03;
 public class FactorialLogic {
 
     public static int calcFactorial(int number) {
-        if (number == 0 || number == 1) {
+        if (number < 0) {
             return 1;
         }
         int result = 1;
-        for (int i = 1; i <= number; i++) {
+        for (int i = 2; i <= number; i++) {
             result *= i;
         }
         return result;
