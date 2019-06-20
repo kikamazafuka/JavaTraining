10a031b2f665116d596e716acbdc49baba7ab7e1
kikamazafuka <uncelfuka@gmail.com>
2019-06-21 01:23:27 +0300

    Add files via upload

---
+++ maintask02/test/by/epam/javatraining/artsem/maintask01/model/VegetableTest.java
@@ -0,0 +1,48 @@
+package by.epam.javatraining.artsem.maintask02.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VegetableTest {
+
+    @Test
+    public void getCalories() {
+    }
+
+    @Test
+    public void setCalories() {
+    }
+
+    @Test
+    public void getWeight() {
+    }
+
+    @Test
+    public void setWeight() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void countCalories() {
+    }
+
+    @Test
+    public void toString() {
+    }
+
+    @Test
+    public void equals() {
+    }
+
+    @Test
+    public void hashCode() {
+    }
+}
