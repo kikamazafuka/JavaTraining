e09114822da5769390f3bf8eda77f49c350b2530
kikamazafuka <uncelfuka@gmail.com>
2019-06-21 01:24:31 +0300

    Add files via upload

---
+++ maintask02/test/by/epam/javatraining/artsem/maintask01/model/SaladsTest.java
@@ -0,0 +1,28 @@
+package by.epam.javatraining.artsem.maintask02.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SaladsTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setname() {
+    }
+
+    @Test
+    public void countSaladCalories() {
+    }
+
+    @Test
+    public void getIngredients() {
+    }
+
+    @Test
+    public void addIngredient() {
+    }
+}
