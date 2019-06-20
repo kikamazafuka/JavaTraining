bba74d387789c1031801224a928779d58934079a
kikamazafuka <uncelfuka@gmail.com>
2019-06-20 21:32:38 +0300

    Update Vegetable.java

--- maintask02/src/by/epam/javatraining/artsem/model/Vegetable.java
+++ maintask02/src/by/epam/javatraining/artsem/model/Vegetable.java
@@ -1,4 +1,14 @@
+/** Chief aplication. Create Salad
+ * @author Artur Semenas
+ * @version 1.0 17.06.2019
+ *class describes Cucumber
+ *
+ *
+ * */
+
 package by.epam.javatraining.artsem.maintask02.model;
+
+import java.util.Objects;
 
 public class Vegetable {
 
@@ -58,7 +68,24 @@ public class Vegetable {
         return calories * weight / 100.0;
     }
 
+    @Override
     public String toString() {
-        return "" + name + " " + weight + " gramms " + "contains " + countCalories() + " kcal";
+        return " " + name + " " + weight + " gramms " + "contains " + countCalories() + " kcal";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vegetable vegetable = (Vegetable) o;
+        return Double.compare(vegetable.calories, calories) == 0 &&
+                Double.compare(vegetable.weight, weight) == 0 &&
+                Objects.equals(name, vegetable.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(calories, weight, name);
     }
 }
+
